7edef8ffe1d748ad5808ed9f6cbf5c1df99508f1
SISU12 <136362802+SISU12@users.noreply.github.com>
2023-07-18 19:11:58 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,161 @@
+// Math operation functions
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) {
+    displayError('Cannot divide by zero!');
+    return NaN;
+  }
+  return a / b;
+}
+
+// Get calculator element
+const calculatorElement = document.querySelector('.calculator');
+
+// Get input elements
+const widthInput = document.getElementById('width-input');
+const heightInput = document.getElementById('height-input');
+const applyButton = document.getElementById('apply-btn');
+
+// Apply button click event listener
+applyButton.addEventListener('click', () => {
+  const width = parseInt(widthInput.value);
+  const height = parseInt(heightInput.value);
+
+  if (!isNaN(width) && !isNaN(height)) {
+    calculatorElement.style.width = `${width}px`;
+    calculatorElement.style.height = `${height}px`;
+  }
+});
+
+// Variables for calculator operation
+let expression = '';
+let shouldResetDisplay = false;
+
+// Display element
+const displayElement = document.getElementById('display');
+
+// Helper functions
+function updateDisplay(value) {
+  displayElement.textContent = value;
+}
+
+function displayError(message) {
+  updateDisplay('Error: ' + message);
+}
+
+// Event listeners for number buttons
+document.querySelectorAll('.number').forEach((button) => {
+  button.addEventListener('click', () => {
+    if (shouldResetDisplay) {
+      expression = '';
+      shouldResetDisplay = false;
+    }
+    const digit = button.textContent;
+    expression += digit;
+    updateDisplay(expression);
+  });
+});
+
+// Event listeners for operator buttons
+document.querySelectorAll('.operator').forEach((button) => {
+  button.addEventListener('click', () => {
+    if (shouldResetDisplay) {
+      shouldResetDisplay = false;
+    }
+    const operator = button.textContent;
+    expression += ` ${operator} `;
+    updateDisplay(expression);
+  });
+});
+
+// Event listener for the equals button
+document.getElementById('equals').addEventListener('click', () => {
+  try {
+    const result = evaluateExpression();
+    updateDisplay(result);
+    expression = String(result);
+    shouldResetDisplay = true;
+  } catch (error) {
+    displayError('Invalid expression');
+  }
+});
+
+// Event listener for the clear button
+document.getElementById('clear').addEventListener('click', () => {
+  expression = '';
+  shouldResetDisplay = false;
+  updateDisplay('');
+});
+
+// Event listener for the backspace button
+document.getElementById('backspace').addEventListener('click', () => {
+  const currentDisplay = displayElement.textContent;
+  expression = currentDisplay.slice(0, -1);
+  updateDisplay(expression);
+});
+
+// Event listener for the decimal button
+document.getElementById('decimal').addEventListener('click', () => {
+  if (!expression.includes('.')) {
+    expression += '.';
+    updateDisplay(expression);
+  }
+});
+
+// Helper function to evaluate the expression using Shunting Yard algorithm
+function evaluateExpression() {
+  const operators = {
+    '+': { precedence: 1, func: add },
+    '-': { precedence: 1, func: subtract },
+    '*': { precedence: 2, func: multiply },
+    '/': { precedence: 2, func: divide },
+  };
+
+  const outputQueue = [];
+  const operatorStack = [];
+
+  expression.split(' ').forEach((token) => {
+    if (!isNaN(token)) {
+      outputQueue.push(parseFloat(token));
+    } else if (token in operators) {
+      const operator1 = token;
+      let operator2 = operatorStack[operatorStack.length - 1];
+      while (
+        operator2 in operators &&
+        operators[operator1].precedence <= operators[operator2].precedence
+      ) {
+        outputQueue.push(operatorStack.pop());
+        operator2 = operatorStack[operatorStack.length - 1];
+      }
+      operatorStack.push(operator1);
+    }
+  });
+
+  while (operatorStack.length > 0) {
+    outputQueue.push(operatorStack.pop());
+  }
+
+  const resultStack = [];
+  outputQueue.forEach((token) => {
+    if (!isNaN(token)) {
+      resultStack.push(token);
+    } else if (token in operators) {
+      const [b, a] = [resultStack.pop(), resultStack.pop()];
+      const result = operators[token].func(a, b);
+      resultStack.push(result);
+    }
+  });
+
+  return resultStack[0];
+}
